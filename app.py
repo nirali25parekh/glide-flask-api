8ba8ee986a2da5d3be563869fe59d11aecbb8df1
Nirali Parekh <nirali25parekh@gmail.com>
2020-08-10 21:44:17 +0530

    heroku setup 11

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ import json
 import os
 
 # TODO: uncomment this for twitter analysis2
-# import twitter_folder_2.twitter_analysis as ta     # for twitter sentiment analysis
+import twitter_folder_2.twitter_analysis as ta     # for twitter sentiment analysis
 
 # TODO: uncomment this for reddit mining
-# import reddit_folder.reddit_mining as rm        # for mining reddit
+import reddit_folder.reddit_mining as rm        # for mining reddit
 
 # TODO: uncomment this for video indexing
-# import video_indexer_folder.video_indexing as vi       # for mining reddit
+import video_indexer_folder.video_indexing as vi       # for mining reddit
 
 app = Flask(__name__)
 for i in os.environ.keys():
